b917b376ebf695e58706bfd1a700977643c7f735
Mazilu Mircea <mmirceaiulius@gmail.com>
2025-12-22 13:06:01 +0200

    Initial Commit

---
+++ BattleShip/BattleShipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleShipApp.swift
+//  BattleShip
+//
+//  Created by Mircea Mazilu on 22.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BattleShipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
